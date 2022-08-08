185bf3ad3ef6b1eb50d1ceff8e5013ab69e52af8
Luiz Sabiano <luizsabiano@gmail.com>
2022-08-08 09:17:07 -0300

    created Mentoria and Curso Class

--- src/main/java/br/com/dio/desafio/Main.java
+++ src/main/java/br/com/dio/desafio/Main.java
@@ -1,8 +1,37 @@
 package br.com.dio.desafio;
 
+import br.com.dio.desafio.dominio.Curso;
+import br.com.dio.desafio.dominio.Mentoria;
+
+import java.time.LocalDate;
+
 public class Main {
     public static void main(String[] args) {
 
-        System.out.println("Hello world!");
+        Curso curso1 = new Curso();
+        Curso curso2 = new Curso();
+        Mentoria mentoria = new Mentoria();
+
+        curso1.setTitulo("Curso Java");
+        curso1.setDescricao("Curso de programação intermediário utilizando linguagem Java");
+        curso1.setCargaHoraria(8);
+
+
+        curso2.setTitulo("Curso Js");
+        curso2.setDescricao("Curso de programação intermediário utilizando linguagem Js");
+        curso2.setCargaHoraria(4);
+
+        mentoria.setTitulo("Mentoria Java");
+        mentoria.setDescricao("Descricao Mentoria Java");
+        mentoria.setData(LocalDate.now());
+
+
+
+        System.out.println(curso1);
+        System.out.println(curso2);
+        System.out.println(mentoria);
+
+
+
     }
 }
